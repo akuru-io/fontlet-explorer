4d7bbba506df31dc61a965cb975f5aef7805505b
thinkholic <mailtokasun@gmail.com>
2018-08-22 00:28:29 +0530

    Add basic font install script (darwin)

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,27 +1,54 @@
 import React from 'react';
 
+const sudo = window.require('sudo-prompt');
+const os = window.require("os");
+
 const fonts = [
   {
     id: 1,
     name: "Abhaya Libre",
     version: "1.0.1",
-    publisher: "mooniak"
+    publisher: "mooniak",
+    url: "url goes here"
   },
   {
     id: 2,
     name: "Malithi Web",
     version: "1.0.2",
-    publisher: "Pushpananda Ekanayake"
+    publisher: "Pushpananda Ekanayake",
+    url: "url goes here"
   }
 ];
 
-const FontItem = ({ id, name, version, publisher}) => (
+function installFont(url) {
+  // Detect O/S
+  console.log(os.type(), os.platform());
+
+  // Directory/File paths
+  const fontFilePath = "/Users/jarvis/Dev/apps/fontcase-apps/fontcase-explorer/_tmp/Athena.ttf";
+  const localFontsDirPath = "~/Library/Fonts/";
+
+  // TODO:
+  // Here, based on the O/S do run the terminal commands in `sudo.exec(______)`
+
+  const options = {
+    name: 'fontcase'
+  };
+  sudo.exec(`cp ${fontFilePath} ${localFontsDirPath}`, options,
+    function(error, stdout, stderr) {
+      if (error) throw error;
+      console.log('stdout: ' + stdout);
+    }
+  );
+}
+
+const FontItem = ({ id, name, version, publisher, url}) => (
   <li key={id}>
     <div>
         <p>{name} | v{version}</p>
         <p>{publisher}</p>
       
-        <button>Install</button>
+        <button onClick={() => installFont(url)}>Install</button>
     </div>
   </li>
 );
