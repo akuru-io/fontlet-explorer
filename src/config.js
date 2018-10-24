737aa2187b1c52ae821dcb9583aa90ff72565da8
thinkholic <mailtokasun@gmail.com>
2018-10-24 20:51:52 +0530

    Update config.

--- src/config.js
+++ src/config.js
@@ -3,3 +3,8 @@ export const API_BASE_URL =
   "https://wt-a14a4787565a4dc11131e5ec3b0a534a-0.sandbox.auth0-extend.com/fontcase-user-api/";
 export const FL_RESOURCE_URL =
   "https://raw.githubusercontent.com/fontlet/assets/master/fontlet.json";
+
+export default {
+  API_BASE_URL,
+  FL_RESOURCE_URL
+}
