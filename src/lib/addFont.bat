9d14533c22b95818585c78253819339a80857f71
thinkholic <mailtokasun@gmail.com>
2018-10-27 21:26:39 +0530

    Remove unwanted resources

--- src/lib/addFont.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-@echo path of thr font file or folder %1 
-PowerShell Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Bypass
-PowerShell -command "& '%~dp0add-font.ps1' -path '%1%'"
--- src/lib/removeFont.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-@echo font name is(bat) %1 
-PowerShell Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Bypass
-PowerShell -command  "& '%~dp0remove-font.ps1' -file '%1'"
