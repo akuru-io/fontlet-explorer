056c637c17560ac8013024a3501292f274476e49
thinkholic <mailtokasun@gmail.com>
2018-10-27 21:25:50 +0530

    Clean Gallery Component

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -3,12 +3,6 @@ import styled from "styled-components";
 import { Switch, Card, Elevation } from "@blueprintjs/core";
 import find from "lodash/find";
 
-// import fonts from "../data/fonts";
-// import installFont from "../lib/installFont";
-// import uninstallFont from "../lib/uninstallFonts";
-
-// import dbFonts from "../lib/db/fonts";
-
 // Styles
 const Wrapper = styled.div`
   height: 100vh;
@@ -84,179 +78,13 @@ class Gallery extends Component {
     waiting: true
   };
 
-  // constructor(props) {
-  //   super(props);
-
-  //   this.state = {
-  //     waiting: true
-  //     loading: false,
-  //     fontData: [],
-  //     loadingFontId: ""
-  //   };
-
-  //   this.addRemoveFont = this.addRemoveFont.bind(this);
-  // }
-
   componentDidMount() {
     setTimeout(() => {
       this.setState(() => ({
         waiting: false
       }));
     }, 500);
-
-    // fonts.forEach(f => {
-    //   dbFonts.find({ id: f.id }, (err, resp) => {
-    //     if (err) {
-    //       // eslint-disable-next-line no-unused-vars
-    //       const Alert = new Notification("Oops!.. Something went wrong!");
-    //       return;
-    //     }
-
-    //     const font = (resp && resp[0]) || {};
-    //     // TODO: Fix lint issue
-    //     // eslint-disable-next-line
-    //     this.setState({
-    //       fontData: [...this.state.fontData, { ...f, ...font }]
-    //     });
-    //   });
-    // });
   }
-
-  // addRemoveFont = async (installing, id) => {
-  //   const { fontData } = this.state;
-  //   const fontsToBeInstalled = fontData.find(f => f.id === id);
-
-  //   this.setState({ loading: true, loadingFontId: id });
-
-  //   if (installing) {
-  //     try {
-  //       const fontList = fontsToBeInstalled.list;
-  //       installFont(fontList, error => {
-  //         if (error) {
-  //           // eslint-disable-next-line no-unused-vars
-  //           const Alert = new Notification("Oops!.. Font installing failed!");
-  //           this.setState({ loading: false, loadingFontId: "" });
-  //           return;
-  //         }
-
-  //         // Update states
-  //         const newFontData = fontData.map(font => {
-  //           if (font.id === id) {
-  //             return {
-  //               ...font,
-  //               installed: true
-  //             };
-  //           }
-  //           return font;
-  //         });
-
-  //         this.setState({
-  //           loading: false,
-  //           loadingFontId: "",
-  //           fontData: newFontData
-  //         });
-  //         // eslint-disable-next-line no-unused-vars
-  //         const Alert = new Notification("Font installed successfully! ");
-
-  //         // Update storage
-  //         dbFonts.update(
-  //           { id },
-  //           { type: "fonts", id, installed: true },
-  //           dbErr => {
-  //             if (dbErr) {
-  //               // eslint-disable-next-line
-  //               const Alert = new Notification(
-  //                 "Oops!.. Something wrong in updating database."
-  //               );
-  //             }
-  //           }
-  //         );
-  //       });
-  //     } catch (error) {
-  //       // eslint-disable-next-line
-  //       const Alert = new Notification("Oops!.. Font installing failed!");
-  //       this.setState({ loading: false, loadingFontId: "" });
-  //     }
-  //   } else {
-  //     uninstallFont(fontsToBeInstalled, error => {
-  //       if (error) {
-  //         // eslint-disable-next-line no-unused-vars
-  //         const Alert = new Notification("Oops!.. Font uninstalling failed!");
-  //         this.setState({ loading: false, loadingFontId: "" });
-  //         return;
-  //       }
-
-  //       // Update states
-  //       const newFontData = fontData.map(font => {
-  //         if (font.id === id) {
-  //           return {
-  //             ...font,
-  //             installed: false
-  //           };
-  //         }
-  //         return font;
-  //       });
-  //       this.setState({
-  //         loading: false,
-  //         loadingFontId: "",
-  //         fontData: newFontData
-  //       });
-
-  //       // Update storage
-  //       dbFonts.update(
-  //         { id },
-  //         { type: "fonts", id, installed: false },
-  //         dbErr => {
-  //           if (dbErr) {
-  //             // eslint-disable-next-line no-unused-vars
-  //             const Alert = new Notification(
-  //               "Oops!.. Something wrong in updating database."
-  //             );
-  //           }
-  //         }
-  //       );
-  //     });
-  //   }
-  // };
-
-  // FontItem = ({ id, version, installed, fontImage, fontVariants }) => {
-  //   const { loading, loadingFontId } = this.state;
-
-  //   return (
-  //     <CardContent className="card-style" key={id}>
-  //       <Content elevation={Elevation.TWO}>
-  //         <FontImage src={fontImage} />
-
-  //         {loading &&
-  //           loadingFontId === id && (
-  //             <div className="bp3-progress-bar bp3-intent-primary">
-  //               <div className="bp3-progress-meter" />
-  //             </div>
-  //           )}
-
-  //         <SettingsContent>
-  //           <VersionContent>
-  //             <VersionDetails>
-  //               <Version>v {version}</Version>
-  //               <Variant>Number of styles : {fontVariants}</Variant>
-  //             </VersionDetails>
-  //           </VersionContent>
-
-  //           <ToggleButtonWrapper>
-  //             <Switch
-  //               className="switch-style"
-  //               checked={installed || false}
-  //               large
-  //               onChange={() => {
-  //                 this.addRemoveFont(!installed, id);
-  //               }}
-  //             />
-  //           </ToggleButtonWrapper>
-  //         </SettingsContent>
-  //       </Content>
-  //     </CardContent>
-  //   );
-  // };
 
   handleSwitchAction = (font, uninstall) => {
     const { installFont, uninstallFont } = this.props;
