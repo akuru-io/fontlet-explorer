d3501cf9794c861e8ef96294aede061821dfd3f3
unknown <c4c5nf3@gmail.com>
2018-11-13 17:29:58 +0530

    windows - add support to multiple fonts install-(ps script version)

--- src/actions/fonts/installer/win.js
+++ src/actions/fonts/installer/win.js
@@ -4,40 +4,55 @@ import { removeLastDirPartOf } from "../../_utils";
 // const sudo = window.require("sudo-prompt");
 const fs = window.require("fs");
 const request = window.require("request");
-// const { exec } = window.require("child_process");
+
 
 const winInstaller = async (font, cb) => {
   // TODO: IMPLEMENTS MULTI_FONTS INSTALL MECHANISM like in Mac/Lin methods ***
   const fontStyles = font.fontStyles || [];
-  const { fontUrl } = fontStyles[0];
 
-  // get filename
-  const fileName = fontUrl.substr(fontUrl.lastIndexOf("/") + 1);
+  // create root folder to hold all fonts inside the app root
+      if (!fs.existsSync(`${appUserDir}/fontCache/`)){
+        fs.mkdirSync(`${appUserDir}/fontCache/`);  
+      }
+  // generate font temp folder name     
+  const tempFolderName = '_' + Math.random().toString(36).substr(2, 9);
+  
 
-  const pathToBeDownload = `${appUserDir}\\${fileName}`; // TODO add folde name
-  // font to save downloaded fonts
-  // download font file to user app directory
-  await new Promise(resolve =>
-    request(fontUrl)
-      .pipe(fs.createWriteStream(pathToBeDownload))
-      .on("finish", resolve)
-  );
+  // create temp folder to download font family
+  const folderToBeDownload = `${appUserDir}/fontCache/${tempFolderName}/`
+  if (!fs.existsSync(folderToBeDownload)){
+    fs.mkdirSync(folderToBeDownload);  
+  }
+
+  // downoload all fonts to temp folder
+  const fontInstallingQueue = await fontStyles.map(async ({ fontUrl }) => {
+    const fileName = fontUrl.substr(fontUrl.lastIndexOf("/") + 1);
+    const pathToBeDownload = `${folderToBeDownload}${fileName}`;
+    console.log(33,pathToBeDownload)
+    await new Promise(resolve =>
+      request(fontUrl)
+        .pipe(fs.createWriteStream(pathToBeDownload))
+        .on("finish", resolve)
+    );
+    return pathToBeDownload;
+  });
 
   // script for install font in windows
   let resolveAppRoot = appRoot;
   let addFont = `${appRoot}\\src\\etc\\addFont.bat`;
+  console.log("appRoot",appRoot)
 
   if (
     resolveAppRoot.substr(resolveAppRoot.lastIndexOf("\\") + 1) === "app.asar"
   ) {
     resolveAppRoot = removeLastDirPartOf(resolveAppRoot);
-    resolveAppRoot = removeLastDirPartOf(resolveAppRoot.slice(0, -1));
+    resolveAppRoot = removeLastDirPartOf(resolveAppRoot.slice(0, -1));      
     addFont = `${resolveAppRoot}\\src\\etc\\addFont.bat`;
   }
 
-  const fileNameOrfolder = pathToBeDownload;
+  const fileNameOrfolder = folderToBeDownload;
 
-  // Install
+    //Install
   const { spawn } = window.require("child_process");
   const ls = spawn("cmd.exe", ["/c", addFont, fileNameOrfolder]); // run script font add bat script
 
