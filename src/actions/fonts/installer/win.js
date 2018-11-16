38398c2f36263050b833109dca639d793fc8d225
unknown <c4c5nf3@gmail.com>
2018-11-16 18:34:30 +0530

    windows installer and unstaller with FontReg.exe

--- src/actions/fonts/installer/win.js
+++ src/actions/fonts/installer/win.js
@@ -1,5 +1,5 @@
 import { appUserDir, appRoot,localFontsDirPaths } from "../../../config";
-import { removeLastDirPartOf } from "../../_utils";
+
 
 // const sudo = window.require("sudo-prompt");
 const sudo = window.require("sudo-prompt");
@@ -17,21 +17,16 @@ const winInstaller = async (font, cb) => {
       if (!fs.existsSync(`${appUserDir}/fontCache/`)){
         fs.mkdirSync(`${appUserDir}/fontCache/`);  
       }
-  // generate font temp folder name     
-  const tempFolderName = '_' + Math.random().toString(36).substr(2, 9);
-  
 
-  // create temp folder to download font family
-  const folderToBeDownload = `${appUserDir}/fontCache/${tempFolderName}/`
-  if (!fs.existsSync(folderToBeDownload)){
-    fs.mkdirSync(folderToBeDownload);  
-  }
+      
+
+
 
    // downoload all fonts to temp folder
   const fontInstallingQueue = await fontStyles.map(async ({ fontUrl }) => {
     const fileName = fontUrl.substr(fontUrl.lastIndexOf("/") + 1);
-    const pathToBeDownload = `${folderToBeDownload}${fileName}`;
-    console.log(33,pathToBeDownload)
+    const pathToBeDownload = `${appUserDir}/fontCache/${fileName}`;
+
     await new Promise(resolve =>
       request(fontUrl)
         .pipe(fs.createWriteStream(pathToBeDownload))
@@ -42,53 +37,26 @@ const winInstaller = async (font, cb) => {
 
   // script for install font in windows
   let resolveAppRoot = appRoot;
-  let addFont = `${appRoot}\\src\\etc\\addFont.bat`;
+
+  let fontReg = `${appUserDir}\\fontCache\\FontReg.exe`;
   console.log("appRoot",appRoot)
-
-  if (
-    resolveAppRoot.substr(resolveAppRoot.lastIndexOf("\\") + 1) === "app.asar"
-  ) {
-    resolveAppRoot = removeLastDirPartOf(resolveAppRoot);
-    resolveAppRoot = removeLastDirPartOf(resolveAppRoot.slice(0, -1));      
-    addFont = `${resolveAppRoot}\\src\\etc\\addFont.bat`;
-  }
-
-  const fileNameOrfolder = folderToBeDownload;
-
   
-  Promise.all(fontInstallingQueue).then(paths => {
-    const fontsFilePath = paths.join(" ");
-    const localFontsDirPath = localFontsDirPaths.windows;
 
-    const options = { name: "fontlet", cachePassword: true };
-
-    // TODO: xx
-    exec(`mkdir -p ${localFontsDirPath}`, {}, error => {
-      if (error) {
-        cb({ message: "Installing failed!", params: error }, null);
-        return;
-      }
-
-      exec(
-        `cp ${fontsFilePath} ${localFontsDirPath}`,
+  const options = { name: "fontlet", cachePassword: true };
+  // install font using fontReg.exe
+    sudo.exec(
+        ` ${fontReg} /copy`,
         options,
         fontCopyError => {
           if (fontCopyError) {
             cb({ message: "Installing failed!", params: fontCopyError }, null);
             return;
           }
-
-          sudo.exec(`fc-cache -f -v`, options, (fCacheError, fCacheStdout) => {
-            if (fCacheError) {
-              cb({ message: "Installing failed!", params: fCacheError }, null);
-              return;
-            }
-            cb(null, fCacheStdout);
-          });
+     
         }
       );
-    });
-  });
+  
+
 
 
 };
--- src/actions/fonts/uninstaller/win.js
+++ src/actions/fonts/uninstaller/win.js
@@ -1,7 +1,47 @@
-const winUninstaller = async (font, cb) => {
-  // TODO
-  // Implements the windows uninstaller
-  cb({ message: "NOT_SUPPORTED_YET", params: font }, null);
+import {localFontsDirPaths ,appUserDir } from "../../../config";
+const sudo = window.require("sudo-prompt");
+
+
+
+const windowsUninstaller = async (font, cb) => {
+  const fontStyles = font.fontStyles || [];
+
+  let fontReg = `${appUserDir}\\fontCache\\FontReg.exe`;
+
+  const filesNames = fontStyles.map(({ fontUrl }) => {
+    const splittedUrl = fontUrl.split("/").reverse();
+    return splittedUrl[0];
+  });
+
+  const filePaths = filesNames
+    .map(fileName => {
+      
+      return `${localFontsDirPaths.windows}/${fileName}`;
+    })
+    .join(" ");
+
+  const options = { name: "fontlet", cachePassword: true };
+  //delete font file from source
+  sudo.exec(`Remove-Item –path ${filePaths}`, options, (error /* , stdout */) => {
+    if (error) {
+      cb({ message: "Uninstalling failed!", params: error }, null);
+      return;
+    }
+
+    //refresh font cache using fontReg
+    sudo.exec(
+      `${fontReg}`,
+      options,
+      fontCopyError => {
+        if (fontCopyError) {
+          cb({ message: "unstalling failed!", params: fontCopyError }, null);
+          return;
+        }
+
+     
+      }
+    );
+  });
 };
 
-export default winUninstaller;
+export default windowsUninstaller;
