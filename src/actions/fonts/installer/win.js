ca8000f303d0784dae460fb053b2699f5787ac01
thinkholic <mailtokasun@gmail.com>
2018-12-21 15:03:08 +0530

    Fix win application flow

--- src/actions/fonts/installer/win.js
+++ src/actions/fonts/installer/win.js
@@ -1,57 +1,32 @@
-import { appUserDir, appRoot } from "../../../config";
-import { removeLastDirPartOf } from "../../_utils";
+import { appUserDir, fontRegExecPath } from "../../../config";
+import { runCmd } from "../../_utils";
 
-// const sudo = window.require("sudo-prompt");
 const fs = window.require("fs");
 const request = window.require("request");
-// const { exec } = window.require("child_process");
 
-const winInstaller = async (font, cb) => {
-  // TODO: IMPLEMENTS MULTI_FONTS INSTALL MECHANISM like in Mac/Lin methods ***
-  const fontStyles = font.fontStyles || [];
-  const { fontUrl } = fontStyles[0];
+const install = async font => {
+  try {
+    const fontStyles = font.fontStyles || [];
+    const tmpFontPaths = [];
+    const fontInstallingQueue = fontStyles.map(async ({ fontUrl }) => {
+      const fileName = fontUrl.substr(fontUrl.lastIndexOf("/") + 1);
+      const pathToBeDownload = `${appUserDir}/${fileName}`;
+      tmpFontPaths.push(pathToBeDownload);
+      await new Promise(resolve =>
+        request(fontUrl)
+          .pipe(fs.createWriteStream(pathToBeDownload))
+          .on("finish", resolve)
+      );
+    });
 
-  // get filename
-  const fileName = fontUrl.substr(fontUrl.lastIndexOf("/") + 1);
+    await Promise.all(fontInstallingQueue);
 
-  const pathToBeDownload = `${appUserDir}\\${fileName}`; // TODO add folde name
-  // font to save downloaded fonts
-  // download font file to user app directory
-  await new Promise(resolve =>
-    request(fontUrl)
-      .pipe(fs.createWriteStream(pathToBeDownload))
-      .on("finish", resolve)
-  );
-
-  // script for install font in windows
-  let resolveAppRoot = appRoot;
-  let addFont = `${appRoot}\\src\\etc\\addFont.bat`;
-
-  if (
-    resolveAppRoot.substr(resolveAppRoot.lastIndexOf("\\") + 1) === "app.asar"
-  ) {
-    resolveAppRoot = removeLastDirPartOf(resolveAppRoot);
-    resolveAppRoot = removeLastDirPartOf(resolveAppRoot.slice(0, -1));
-    addFont = `${resolveAppRoot}\\src\\etc\\addFont.bat`;
+    const cmd = `cp ${fontRegExecPath}/FontReg.exe ${appUserDir}/ && ${appUserDir}FontReg.exe /move`;
+    await runCmd(cmd);
+    return font;
+  } catch (error) {
+    throw new Error(error);
   }
-
-  const fileNameOrfolder = pathToBeDownload;
-
-  // Install
-  const { spawn } = window.require("child_process");
-  const ls = spawn("cmd.exe", ["/c", addFont, fileNameOrfolder]); // run script font add bat script
-
-  ls.stdout.on("data", data => {
-    cb(null, data);
-  });
-
-  ls.stderr.on("data", data => {
-    cb({ message: "Installing failed!", params: data }, null);
-  });
-
-  ls.on("exit", code => {
-    cb({ message: "Installing failed!", params: code }, null);
-  });
 };
 
-export default winInstaller;
+export default install;
--- src/actions/fonts/updater/win.js
+++ src/actions/fonts/updater/win.js
@@ -1,24 +1,14 @@
 import uninstall from "../uninstaller/win";
 import install from "../installer/win";
 
-const winUpdater = async (font, cb) => {
-  // Uninstall font first
-  uninstall(font, uninsErr => {
-    if (uninsErr) {
-      cb({ message: "Updating failed!", params: uninsErr }, null);
-      return;
-    }
-
-    // Update new version
-    install(font, insErr => {
-      if (insErr) {
-        cb({ message: "Updating failed!", params: insErr }, null);
-        return;
-      }
-
-      cb(null, font);
-    });
-  });
+const update = async font => {
+  try {
+    await uninstall(font);
+    await install(font);
+    return font;
+  } catch (error) {
+    throw new Error(error);
+  }
 };
 
-export default winUpdater;
+export default update;
